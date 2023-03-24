2022eef542a60499a57f49ee25fb0db7a3da1c5d
Rey Anthony Masilang <rey.masilang@gmail.com>
2023-03-24 21:52:42 +0800

    Refactored

--- src/etl/sync.py
+++ src/etl/sync.py
@@ -8,150 +8,213 @@ from datetime import datetime, timedelta
 from src.utils.pse_edge import get_listed_companies, get_stock_data
 from src.utils.postgres import query
 
-def get_ts():
-    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-
-# Extract complete list of companies
-company_extract_ts = get_ts()
-companies_df = get_listed_companies()
-n_symbols = companies_df.shape[0]
-
-# Load to database
-print('Inserting rows to database.')
-
-BATCH_SIZE = 100
-INSERT_STMT_TEMPLATE = r"""
-    INSERT INTO pse.company (symbol, company_name, sector, subsector, listing_date, extracted_at)
-    VALUES {}
-    ON CONFLICT (symbol)
-    DO UPDATE SET
-        symbol = EXCLUDED.symbol,
-        company_name = EXCLUDED.company_name,
-        sector = EXCLUDED.sector,
-        subsector = EXCLUDED.subsector,
-        listing_date = EXCLUDED.listing_date,
-        extracted_at = EXCLUDED.extracted_at;
-
-    COMMIT;
-"""
-
-
-rows_to_insert = []
-for idx in range(n_symbols):
-    symbol = companies_df.iloc[idx].loc['symbol']
-    company_name = companies_df.iloc[idx].loc['company_name']
-    sector = companies_df.iloc[idx].loc['sector']
-    subsector = companies_df.iloc[idx].loc['subsector']
-    listing_date = companies_df.iloc[idx].loc['listing_date']
-    extracted_at = company_extract_ts
+def insert_companies_to_db(df: pd.DataFrame, batch_size: int = 100) -> None:
+    """Inserts company records to database.
     
-    row_str = "('{}','{}','{}','{}','{}','{}')".format(
-        symbol, 
-        company_name.replace('\'','\'\''), 
-        sector, 
-        subsector, 
-        listing_date,
-        extracted_at
-    )
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        A DataFrame of companies which are part of the Philippine Stock Exchange.
+      
+    batch_size : int, default 100
+        The number of records to insert at a time.
+      
+    Returns
+    -------
+    None
     
-    rows_to_insert.append(row_str)
+    """
     
-    if (len(rows_to_insert) == BATCH_SIZE) or (idx+1 == n_symbols):
-        print('  {} out of {}'.format(idx+1, n_symbols))
-        stmt = INSERT_STMT_TEMPLATE.format(',\n           '.join(rows_to_insert))
-        query(stmt=stmt, retrieve_result=False)
+    n_symbols = df.shape[0]
+    
+    print('Inserting rows to database.')
 
-        rows_to_insert = []
+    INSERT_STMT_TEMPLATE = r"""
+        INSERT INTO pse.company (symbol, company_name, sector, subsector, listing_date, extracted_at)
+        VALUES {tuples}
+        ON CONFLICT (symbol)
+        DO UPDATE SET
+            symbol = EXCLUDED.symbol,
+            company_name = EXCLUDED.company_name,
+            sector = EXCLUDED.sector,
+            subsector = EXCLUDED.subsector,
+            listing_date = EXCLUDED.listing_date,
+            extracted_at = EXCLUDED.extracted_at;
 
+        COMMIT;
+    """
+
+    rows_to_insert = []
+    for idx in range(n_symbols):
+        symbol, company_name, sector, subsector, listing_date, extracted_at = \
+            df.iloc[idx].loc[['symbol', 'company_name', 'sector', 'subsector', 'listing_date', 'extracted_at']]
+
+        row_str = f"('{symbol}','{company_name}','{sector}','{subsector}','{listing_date}','{extracted_at}')"
+        rows_to_insert.append(row_str)
+
+        if (len(rows_to_insert) == batch_size) or (idx+1 == n_symbols):
+            print(f'  {idx+1} out of {n_symbols}')
+            stmt = INSERT_STMT_TEMPLATE.format(tuples=',\n           '.join(rows_to_insert))
+            query(stmt=stmt, retrieve_result=False)
+            rows_to_insert = []
+
+
+def sync_companies() -> pd.DataFrame:
+    """Updates the companies dataset."""
+            
+    # Extract complete list of companies
+    companies_df = get_listed_companies()
+
+    # Load to database
+    insert_companies_to_db(companies_df)
+    
+    return companies_df
+    
+
+def get_latest_date_from_db(symbol : str) -> str:
+    """Extracts the date of the latest price data point for a specified ticker symbol.
+    
+    Parameters
+    ----------
+    symbol : str
+        The ticker symbol of the company.
         
+    Returns
+    -------
+    latest_date : str
+        The date of the latest price data available in the database.
+    
+    """
+
+    # Get latest data
+    stmt = f"""
+        SELECT max(date) AS latest_date
+        FROM pse.daily_stock_price
+        WHERE symbol = '{symbol}';
+    """
+
+    data = query(stmt=stmt)
+    latest_date = data.iloc[0].loc['latest_date']
+    
+    return latest_date
+
+
+def get_new_price_data(symbol: str, lookback_days : int = 0, freshness_days : int = 1) -> pd.DataFrame:
+    """
+    Extracts missing price data for a specific company starting from the
+    latest date in the database up to the current date.
+    
+    Parameters
+    ----------
+    symbol : str
+        The ticker symbol of the company.
+    
+    lookback_days : int, default 0
+        The number of days in the past to re-extract price data for.
         
-# Get latest data
-stmt = r"""
-    SELECT
-          symbol
-        , max(date) AS latest_date
-
-    FROM pse.daily_stock_price
-    GROUP BY symbol
-    ORDER BY symbol
-"""
-
-latest_dates = query(stmt=stmt)
-latest_dates = latest_dates.set_index('symbol')
-latest_dates = latest_dates.latest_date.apply(pd.to_datetime)
-latest_dates = latest_dates.dt.strftime('%Y-%m-%d')
-latest_dates = latest_dates.to_dict()
-
-
-# Extract and load price data
-BATCH_SIZE = 3000
-INSERT_STMT_TEMPLATE = r"""
-    INSERT INTO pse.daily_stock_price (symbol, date, open, high, low, close, extracted_at)
-    VALUES {}
-    ON CONFLICT (symbol,date)
-    DO UPDATE SET
-        "symbol" = EXCLUDED.symbol,
-        "date" = EXCLUDED.date,
-        "open" = EXCLUDED.open,
-        "high" = EXCLUDED.high,
-        "low" = EXCLUDED.low,
-        "close" = EXCLUDED.close,
-        "extracted_at" = EXCLUDED.extracted_at;
-
-    COMMIT;
-"""
-
+    freshness_days : int, default 1
+        The acceptable data delay in number of days. By default, this is set to 1
+        which means data for yesterday is the minimum acceptable value to consider
+        that the price data is already up-to-date.
     
-# Extract all available price data for each company
-for idx in range(n_symbols):
-    symbol = companies_df.iloc[idx].loc['symbol']
-    start_date = latest_dates.get(symbol, None)
+    Returns
+    -------
+    price_df : pandas.DataFrame
+        A dataframe of dates and prices for the specified company.
     
-    print('Extracting price data:  {:6s}  ({} out of {})'.format(symbol, idx+1, n_symbols))
+    """
     
-    price_extract_ts = get_ts()
-    if start_date is None:
-        price_df = get_stock_data(symbol)
+    target_latest_date = (datetime.utcnow() + timedelta(hours=8)).date() - timedelta(days=freshness_days)
+    latest_date = get_latest_date_from_db(symbol)
+    
+    # Skip data extraction if conditions are satisfied
+    if lookback_days==0 and latest_date==target_latest_date:
+        # Return empty data frame.
+        price_df = pd.DataFrame(columns=['symbol','date','open','high','low','close','extracted_at'])
+        print('  Already up-to-date. Skipping.')
+        
     else:
-        start_date = (pd.to_datetime(start_date) + timedelta(days=1)).strftime('%Y-%m-%d')
+        if latest_date is not None: 
+            start_date = (latest_date + timedelta(days=1-lookback_days)).strftime('%Y-%m-%d')
+        else:
+            start_date = None
+            
         price_df = get_stock_data(symbol, start_date=start_date)
+        print(f'  Fetched {price_df.shape[0]} records.')
     
-    # Insert to database
-    n_rows = price_df.shape[0]
+    return price_df
+
+
+def insert_price_data_to_db(df: pd.DataFrame, batch_size: int = 3000) -> None:
+    """Inserts price data records to the database.
     
-    if n_rows == 0:
-        print('  No price data available. Skipping.')
-        
-    else:
-        print('  Inserting rows to database.')
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        A DataFrame of price data to insert to the database.
+      
+    batch_size : int, default 100
+        The number of records to insert at a time.
+      
+    Returns
+    -------
+    None
+    
+    """
+    
+    INSERT_STMT_TEMPLATE = r"""
+        INSERT INTO pse.daily_stock_price (symbol, date, open, high, low, close, extracted_at)
+        VALUES {tuples}
+        ON CONFLICT (symbol,date)
+        DO UPDATE SET
+            "symbol" = EXCLUDED.symbol,
+            "date" = EXCLUDED.date,
+            "open" = EXCLUDED.open,
+            "high" = EXCLUDED.high,
+            "low" = EXCLUDED.low,
+            "close" = EXCLUDED.close,
+            "extracted_at" = EXCLUDED.extracted_at;
 
-        rows_to_insert = []
-        for idy in range(n_rows):
-            p_symbol = price_df.iloc[idy].loc['symbol']
-            p_date = price_df.iloc[idy].loc['date']
-            p_open = price_df.iloc[idy].loc['open']
-            p_high = price_df.iloc[idy].loc['high']
-            p_low = price_df.iloc[idy].loc['low']
-            p_close = price_df.iloc[idy].loc['close']
-            p_extracted_at = price_extract_ts
+        COMMIT;
+    """
+    
+    n_rows = df.shape[0]
+    rows_to_insert = []
+    for idy in range(n_rows):
+        p_symbol, p_date, p_open, p_high, p_low, p_close, p_extracted_at = \
+            df.iloc[idy].loc[['symbol','date','open','high','low','close','extracted_at']]
 
-            row_str = "('{}', '{}', {}, {}, {}, {}, '{}')".format(
-                p_symbol, 
-                p_date, 
-                p_open, 
-                p_high, 
-                p_low, 
-                p_close, 
-                p_extracted_at
-            )
+        row_str = f"('{p_symbol}', '{p_date}', {p_open}, {p_high}, {p_low}, {p_close}, '{p_extracted_at}')"
+        rows_to_insert.append(row_str)
 
-            rows_to_insert.append(row_str)
+        if (len(rows_to_insert) == batch_size) or (idy+1 == n_rows):
+            print(f'  Inserted {n_rows} records.')
+            stmt = INSERT_STMT_TEMPLATE.format(tuples = ',\n           '.join(rows_to_insert))
+            query(stmt=stmt, retrieve_result=False)
+            rows_to_insert = []
+            
+            
+def sync_prices(companies_df : pd.DataFrame) -> None:
+    """Updates price data for all companies."""
 
-            if (len(rows_to_insert) == BATCH_SIZE) or (idy+1 == n_rows):
-                print('    {:6s}  |  {}  | {} out of {}'.format(symbol, p_date, idy+1, n_rows))
-                stmt = INSERT_STMT_TEMPLATE.format(',\n           '.join(rows_to_insert))
-                query(stmt=stmt, retrieve_result=False)
+    n_symbols = companies_df.shape[0]
+    for idx in range(n_symbols):
+        symbol = companies_df.iloc[idx].loc['symbol']
+        print(f'Updating price data:  {symbol:6s}  ({idx+1} out of {n_symbols})')
 
-                rows_to_insert = []
+        # Extract price data
+        price_df = get_new_price_data(symbol)
 
+        # Insert to database
+        n_rows = price_df.shape[0]
+        if n_rows == 0:
+            print('  No new records. Skipping.')
+        else:
+            insert_price_data_to_db(price_df)
+
+
+if __name__ == '__main__':
+    
+    companies_df = sync_companies()
+    _ = sync_prices(companies_df)
