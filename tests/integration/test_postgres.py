bb8c2fe41b8668a9e2b453b96b544636c2fd04dc
Rey Anthony Masilang <rey.masilang@gmail.com>
2023-09-03 23:41:18 +0800

    Removed misc function tests

--- tests/integration/test_postgres.py
+++ tests/integration/test_postgres.py
@@ -23,38 +23,6 @@ def test_db_port_defined():
 def test_db_name_defined():
     assert environ.get('POSTGRES_DB_NAME') is not None
     
-def test_read_sql_file():
-    sql_stmt = 'SELECT 1 AS col;'
-    assert sql_stmt == read_sql_file('tests/integration/sample_sql_file.sql')
-
-def test_render_template():
-    raw_stmt = """
-        SELECT
-              '{{string_value}}' AS {{string_column}}
-            , {{int_value}} AS {{int_column}}
-            
-        FROM {{schema}}.{{table}};
-    """
-    
-    templated_stmt = """
-        SELECT
-              'sample_string' AS my_str_col
-            , 10 AS my_int_col
-            
-        FROM my_schema.my_table;
-    """
-    
-    params = {
-        'schema':'my_schema',
-        'table':'my_table',
-        'string_value':'sample_string',
-        'string_column':'my_str_col',
-        'int_value':10,
-        'int_column':'my_int_col'
-    }
-    
-    assert templated_stmt == render_template(raw_stmt, params)
-    
 def test_query_stmt():
     stmt = 'SELECT 1 AS col;'
     assert query(stmt).to_dict('records') == [{'col':1}]
